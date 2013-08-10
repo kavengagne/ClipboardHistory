b0dd1534ed1453da398fb0603c752ae4458d6074
kavengagne <kaven.gagne@gmail.com>
2013-08-10 19:16:24 -0400

    Added UnitTestHelper to replace UnitTestHelperBase assembly

---
+++ ClipboardHistoryTests/UnitTestHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace ClipboardHistoryTests
+{
+    public class UnitTestHelper
+    {
+        // typeof(ClipboardDataItem),
+        // "GetArrayOfLines",
+        // new object[1] { inputString }
+        public static object RunStaticMethod(Type classType, string methodName, object[] methodParameters)
+        {
+            var method = classType.GetMethod(methodName,
+                    BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public);
+            return method.Invoke(null, methodParameters);
+        }
+
+        // typeof(MyControl),
+        // "AddStringToHistoryCollection",
+        // myControl,
+        // new object[] { inputString }
+        public static object RunInstanceMethod(Type classType, string methodName, object classInstance, object[] methodParameters)
+        {
+            var method = classType.GetMethod(methodName,
+                    BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+            return method.Invoke(classInstance, methodParameters);
+        }
+    }
+}
