f136cefac6c837a34a457785ebeb93ff99fba6a5
Kaven Gagne <kaven.gagne@gmail.com>
2014-05-30 22:38:51 -0400

    updated version number

--- ClipboardHistory/Properties/AssemblyInfo.cs
+++ ClipboardHistory/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ClipboardHistory")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Clipboard History For Visual Studio")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("KavenGagne")]
+[assembly: AssemblyCompany("Kaven Gagne")]
 [assembly: AssemblyProduct("ClipboardHistory")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
@@ -29,8 +29,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 
 
 
